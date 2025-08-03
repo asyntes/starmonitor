eb8afa0eeed93189c08aaa808e7f894b6daea57e
antonio.santese <antonio.santese@cetma.it>
2025-08-03 23:44:50 +0200

    wip legend

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Monitor from './components/Monitor/Monitor';
+import Header from './components/Header/Header';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Header />
       <Monitor />
     </div>
   );
---
+++ src/components/Legend/Legend.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Legend.css';
+
+const Legend: React.FC = () => {
+    return (
+        <div className="service-legend">
+            <div className="legend-title">Service Status</div>
+            <div className="legend-items">
+                <div className="legend-item">
+                    <div className="legend-color active"></div>
+                    <span className="legend-label">Active</span>
+                </div>
+                <div className="legend-item">
+                    <div className="legend-color restricted"></div>
+                    <span className="legend-label">Restricted</span>
+                </div>
+                <div className="legend-item">
+                    <div className="legend-color banned"></div>
+                    <span className="legend-label">Banned</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Legend;
